bbffdb128ce707b2d4d5c6abce0b585f15cd7a2f
Agnes Tiimus <AGNES@Agness-Air.lan>
2020-06-01 12:07:37 +0300

    print hello

--- print.py
+++ print.py
@@ -1,5 +1,6 @@
 print("May the forks be with you")
 print("git pull a day keeps the conflicts away")
 
+print('hello')
 # More github jokes: https://github.com/EugeneKay/git-jokes/blob/lulz/Jokes.txt
 # Feel free to git push any jokes
